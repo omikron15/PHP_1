08c6e0552468aabc0d5b04d3bad21b83336e0166
omikron15 <omikron15@hotmail.co.uk>
2016-04-07 18:32:05 +0100

    second upload attempt 4

--- index.php
+++ index.php
@@ -6,12 +6,31 @@
 <p>
     <?php
 
-   for ($i =1; $i<31; $i++)
-   {
+  $Array_People = array();
 
-       echo "On the " . $i . " day of the month \n";
+    array_push($Array_People, "Person 1");
+    array_push($Array_People, "Person 2");
+    array_push($Array_People, "Person 3");
+    array_push($Array_People, "Person 4");
+    array_push($Array_People, "Person 5");
+    array_push($Array_People, "Person 6");
+    array_push($Array_People, "Person 7");
+    array_push($Array_People, "Person 8");
+    array_push($Array_People, "Person 9");
+    array_push($Array_People, "Person 10");
+    array_push($Array_People, "Person 11");
+    array_push($Array_People, "Person 12");
+    array_push($Array_People, "Person 13");
+    array_push($Array_People, "Person 14");
+    array_push($Array_People, "Person 15");
 
-   }
+    $Spec_Winner = rand(1,15);
+    $Mugs_Winner = rand(1,15);
+    $SausageRolls_Winner = rand(1,15);
+
+    print "The specs winner is " . $Array_People[$Spec_Winner];
+    print "The Mugs winner is " . $Array_People[$Mugs_Winner];
+    print "The Sausage Rolls winner is " . $Array_People[$SausageRolls_Winner];
 
     ?>
 </p>
