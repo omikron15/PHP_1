09b7da02e94ca3629a64acb652ba2abd2bb8f513
omikron15 <omikron15@hotmail.co.uk>
2016-04-07 12:09:40 +0100

    second upload attempt 4

--- index.php
+++ index.php
@@ -6,10 +6,25 @@
 <p>
     <?php
 
-    $myname = "frodo Baggins";
-    $myage  = "111";
+    $myname = "Connor Rose";
+    $myage = "10";
 
-    echo "My name is " . $myname . " and my age is " . $myage;
+    if ($myage < 16)
+    {
+        print "none";
+    }
+    else if ($myage >= 21)
+    {
+        print "You can buy specs, mugs and sausage rolls";
+    }
+    else if($myage <= 18)
+    {
+        print "You can buy specs and mugs";
+    }
+    else if($myage <= 16)
+    {
+        print "You can buy specs";
+    }
 
     ?>
 </p>
